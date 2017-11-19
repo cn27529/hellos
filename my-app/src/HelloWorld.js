cabe17ade53a90b707aafd13665bf2fa02bb0a69
cn27529 <cn27529@hotmail.com>
2017-11-19 22:29:03 +0800

    add static folder

--- my-app/src/HelloWorld.js
+++ my-app/src/HelloWorld.js
@@ -9,8 +9,8 @@ class App extends Component {
 <img src = { logo }
 className = "App-logo"
 alt = "logo" />
-<h1 className = "App-title"> Welcome to React </h1> </header> <p className = "App-intro">To get started, edit <code> src / App.js </code> and save to reload. </p> 
-<h1>hello world </h1>
+<h1 className = "App-title"> Hello React </h1> </header> 
+<p className = "App-intro">To get started, edit <code> src / HelloWorld.js </code> and save to reload. </p> 
 <h2>It is {new Date().toLocaleTimeString()}</h2>
 </div>
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './HelloWorld';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+function tick() {
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+  }
+
+  setInterval(tick, 1000);
+  
 registerServiceWorker();
