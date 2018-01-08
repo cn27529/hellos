210262c082dc5a6216e4beb28c0b50254e582d3b
cn27529 <cn27529@hotmail.com>
2018-01-08 09:19:24 +0800

    docker-netcoer-0108

---
+++ 基於 Docker 的 ASP.NET Core API 開發/WebApiDemo/WebApiDemo/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        public ITodoRepository _TodoRepository { get; set; }
+
+        public ValuesController(ITodoRepository todoRepository)
+        {
+            _TodoRepository = todoRepository;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return _TodoRepository.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}", Name = "GetTodo")]
+        public TodoItem Get(string id)
+        {
+            return _TodoRepository.Find(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]TodoItem value)
+        {
+            if (value == null)
+                return BadRequest();
+
+            _TodoRepository.Add(value);
+
+            //CreatedAtRoute 會建立一個狀態 201 的回應，這是為了符合 HTTP 的標準，也就為通知 Client 端新增資源位於那個位置
+            //Headers： Location →http://localhost:54689/api/Todo/b5cd9427-9db7-492d-9bfc-7bb912d9746a
+            return CreatedAtRoute("GetTodo", new { controller = "Todo", id = value.Key }, value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody]TodoItem value)
+        {
+            if (value == null || value.Key != id)
+                return BadRequest();
+
+            var todo = _TodoRepository.Find(id);
+
+            if (todo == null)
+                return NotFound();
+
+            _TodoRepository.Update(value);
+
+            return new NoContentResult();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = _TodoRepository.Find(id);
+
+            if (todo == null)
+                return NotFound();
+
+            _TodoRepository.Remove(id);
+
+            return new NoContentResult();
+        }
+    }
+}
---
+++ 基於 Docker 的 ASP.NET Core API 開發/WebApiDemo/WebApiDemo/Models/TodoRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Models
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private static ConcurrentDictionary<string, TodoItem> _todos = new ConcurrentDictionary<string, TodoItem>();
+
+        public TodoRepository()
+        {
+            Add(new TodoItem { Name = "Item1" });
+            Add(new TodoItem { Name = "Item2" });
+            Add(new TodoItem { Name = "Item3" });
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            _todos[item.Key] = item;
+        }
+    }
+}
