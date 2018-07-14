778195ba7ae7d907103db2c4c272d8fe0fc5d60a
cn27529 <cn27529@hotmail.com>
2018-07-14 13:50:48 +0800

    rollup.config-me

---
+++ rollup-app/bundle.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var format = _interopDefault(require('date-fns/format'));
+
+var span = document.querySelector('#time-now');
+
+function update() {
+	span.textContent = format(new Date(), 'h:mm:ssa');
+	setTimeout(update, 1000);
+}
+
+// even though Rollup is bundling all your files together, errors and
+// logs will still point to your original source modules
+console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+
+update();
